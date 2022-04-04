76b0cad2fefdcc254a3acf84b3e00b6a54cab079
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-04-04 09:03:33 +0900

    コンポーネントの作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MembersComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
