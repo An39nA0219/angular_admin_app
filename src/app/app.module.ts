be3de3590e279a2ca3e0d72802d66c80ad0a8b39
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-04-04 12:44:38 +0900

    データバインディング

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [AppComponent, MembersComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
