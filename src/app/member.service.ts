869cfaff0cd7424bb8e81822d80ddc0f078daf17
An39nA0219 <an39na10282004@yahoo.co.jp>
2022-04-05 10:22:56 +0900

    詳細データのupdate

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Member } from './member';
@@ -7,27 +7,40 @@ import { MEMBERS } from './mock-members';
 import { catchError, map, tap } from 'rxjs/operators'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MemberService {
   private membersUrl = 'api/members';
+  httpOptions = {
+     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
 
   constructor(
     private http: HttpClient,
     private messageService: MessageService
-  ) { }
+  ) {}
 
   getMembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.membersUrl)
-      .pipe(
-        tap(members => this.log('社員データを取得しました')),
-        catchError(this.handleError<Member[]>('getMembers', []))
-      );
+    return this.http.get<Member[]>(this.membersUrl).pipe(
+      tap((members) => this.log('社員データを取得しました')),
+      catchError(this.handleError<Member[]>('getMembers', []))
+    );
   }
 
   getMember(id: number): Observable<Member> {
-    this.messageService.add(`MemberService: 社員データ(id=${id})を取得しました`);
-    return of(MEMBERS.find(member => member.id === id));
+    const url = `${this.membersUrl}/${id}`;
+    return this.http.get<Member>(url).pipe(
+      tap((_) => this.log(`社員データ(id=${id})を取得しました`)),
+      catchError(this.handleError<Member>(`getMember id=${id}`))
+    );
+  }
+
+  updateMember(member: Member): Observable<any> {
+    return this.http.put(this.membersUrl, member, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`社員データ(id=${member.id})を変更しました`)),
+        catchError(this.handleError<any>('updateMember'))
+      );
   }
 
   private log(message: string) {
@@ -40,6 +53,6 @@ export class MemberService {
 
       this.log(`${operation} 失敗： ${error.message}`);
       return of(result as T);
-    }
+    };
   }
 }
